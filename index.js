119ca1d87ba99cf8fe8f16ea73f0ddd93a1377bf
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-25 12:04:06 +0200

    Nodemon installed

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
-server.listen(9000, () => {
-    console.log('server started on http://localhost:9000');
+server.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`)
 })
